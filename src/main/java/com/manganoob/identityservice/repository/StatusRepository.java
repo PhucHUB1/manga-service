ff043e2785173b734b5e1b5d0b2ea6b0e3aed680
phuch <phuchmth2209055@fpt.edu.vn>
2024-09-27 05:09:41 +0700

    Manga

--- src/main/java/com/manganoob/identityservice/repository/StatusRepository.java
+++
@@ -1,8 +0,0 @@
-package com.manganoob.identityservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatusRepository  extends JpaRepository<Status, String> {
-}
